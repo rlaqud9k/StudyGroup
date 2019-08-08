de30890ae1bc7f960ea6337fa6f4aaff722cd1c1
qudrhks <rlaqud9k@gmail.com>
2019-08-08 23:56:23 +0900

    아 글자깨지는 소리 안나게해라!

--- s.py
+++ s.py
@@ -3,12 +3,35 @@ from bs4 import BeautifulSoup
 from urllib import request
 driver = webdriver.Chrome('D:\chromedriver')
 
-driver.implicitly_wait(3)
+driver.implicitly_wait(5)
 
 driver.get('https://google.com')
 driver.find_element_by_name('q').send_keys('구글뉴스')
+driver.implicitly_wait(3)
 driver.find_element_by_name('btnK').click()
+driver.implicitly_wait(5)
 driver.find_element_by_class_name('LC20lb').click()
-html = driver.current_url
-soup = BeautifulSoup(request.urlopen(html))
-print(soup.text)
+driver.implicitly_wait(5)
+driver.find_element_by_class_name('rdp59b').click()
+
+html = request.urlopen(driver.current_url)
+soup = BeautifulSoup(html.read(), 'html.parser')
+select_title = soup.select('.ipQwMb ')
+with open('d:wnews.txt','at', encoding='utf-8') as file:
+    for i in select_title:
+        file.write(i.text+',\n')
+
+before = select_title[0].text
+while(True):
+    select_title2 = soup.select('.ipQwMb ')
+    latest = select_title2[0].text
+    if(before != latest):
+        before = latest
+        with open('d:wnews.txt', 'at', encoding='utf-8') as file:
+            for i in select_title2:
+                file.write(i.text+',\n')
+
+
+
+
+
